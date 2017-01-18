568646c363834af935fcc1cea8ebde35517e6673
treason-rmbp <ma.tengfei2008@163.com>
2017-01-18 18:52:57 +0800

    整理Util 1.0.4

--- TreCoreLib/src/main/java/com/mjiayou/trecorelib/util/LogUtil.java
+++ TreCoreLib/src/main/java/com/mjiayou/trecorelib/util/LogUtil.java
@@ -3,35 +3,46 @@ package com.mjiayou.trecorelib.util;
 import android.app.Activity;
 import android.util.Log;
 
-import com.mjiayou.trecorelib.manager.ActivityManager;
-
 import java.util.LinkedList;
 import java.util.Stack;
 
 /**
- * 日志输出封装
+ * Log输出封装
  * <p>
  * Created by treason on 16/5/14.
  */
 public class LogUtil {
 
     private static final String TAG = LogUtil.class.getSimpleName();
-    private static final String TAG_TRACE_TIME = TAG + "-TraceTime";
 
-    private static boolean SHOW_LOG = true;
-    private static boolean SHOW_LOG_WITH_PATH = false;
+    private static boolean mShow = true;
+    private static boolean mShowPath = false;
+
+    /**
+     * 配置是否显示LOG
+     */
+    public static void setEnable(boolean show) {
+        mShow = show;
+    }
+
+    /**
+     * 配置是否显示路径
+     */
+    public static void setShowPath(boolean show) {
+        mShowPath = show;
+    }
 
     /**
      * Send a VERBOSE log message.
      */
     public static void v(String tag, String msg) {
-        if (SHOW_LOG) {
+        if (mShow) {
             Log.v(TAG + "-" + tag, buildMessage(msg));
         }
     }
 
     public static void v(String msg) {
-        if (SHOW_LOG) {
+        if (mShow) {
             Log.v(TAG, buildMessage(msg));
         }
     }
@@ -40,13 +51,13 @@ public class LogUtil {
      * Send a DEBUG log message.
      */
     public static void d(String tag, String msg) {
-        if (SHOW_LOG) {
+        if (mShow) {
             Log.d(TAG + "-" + tag, buildMessage(msg));
         }
     }
 
     public static void d(String msg) {
-        if (SHOW_LOG) {
+        if (mShow) {
             Log.d(TAG, buildMessage(msg));
         }
     }
@@ -55,13 +66,13 @@ public class LogUtil {
      * Send an INFO log message.
      */
     public static void i(String tag, String msg) {
-        if (SHOW_LOG) {
+        if (mShow) {
             Log.i(TAG + "-" + tag, buildMessage(msg));
         }
     }
 
     public static void i(String msg) {
-        if (SHOW_LOG) {
+        if (mShow) {
             Log.i(TAG, buildMessage(msg));
         }
     }
@@ -70,13 +81,13 @@ public class LogUtil {
      * Send a WARN log message
      */
     public static void w(String tag, String msg) {
-        if (SHOW_LOG) {
+        if (mShow) {
             Log.w(TAG + "-" + tag, buildMessage(msg));
         }
     }
 
     public static void w(String msg) {
-        if (SHOW_LOG) {
+        if (mShow) {
             Log.w(TAG, buildMessage(msg));
         }
     }
@@ -85,95 +96,90 @@ public class LogUtil {
      * Send an ERROR log message.
      */
     public static void e(String tag, String msg, Throwable throwable) {
-        if (SHOW_LOG) {
+        if (mShow) {
             Log.e(TAG + "-" + tag, buildMessage(msg), throwable);
         }
     }
 
     public static void e(String tag, String msg) {
-        if (SHOW_LOG) {
+        if (mShow) {
             Log.e(TAG + "-" + tag, buildMessage(msg));
         }
     }
 
     public static void e(String msg) {
-        if (SHOW_LOG) {
+        if (mShow) {
             Log.e(TAG, buildMessage(msg));
         }
     }
 
     public static void e(String msg, Throwable throwable) {
-        if (SHOW_LOG) {
+        if (mShow) {
             Log.e(TAG, buildMessage(msg), throwable);
-        }
-    }
-
-    /**
-     * 打印初始化信息
-     */
-    public static void printInit(String module) {
-        if (SHOW_LOG) {
-            Log.i(TAG + "-" + module, "初始化数据 -> " + module);
-        }
-    }
-
-    /**
-     * 打印异常信息
-     */
-    public static void printStackTrace(Throwable throwable) {
-        if (SHOW_LOG) {
-            Log.e(TAG, buildMessage("printStackTrace -> "), throwable);
-            throwable.printStackTrace();
-        }
-    }
-
-    /**
-     * 打印 ActivityManager 栈
-     */
-    public static void printActivityList(LinkedList<Activity> activityList) {
-        if (SHOW_LOG) {
-            StringBuilder builder = new StringBuilder();
-            builder.append(ActivityManager.TAG).append(" | size -> ").append(activityList.size());
-            for (int i = 0; i < activityList.size(); i++) {
-                builder.append("\n").append(i).append(" -> ").append(activityList.get(i).getClass().getSimpleName());
-            }
-            LogUtil.i(ActivityManager.TAG, builder.toString());
         }
     }
 
     /**
      * TraceTime
      */
+    public static final String TAG_TRACE_TIME = "TraceTime";
+
     private static Stack<Long> traceTimeStack = new Stack<>();
 
-    public static void resetTraceTime() {
+    public static void traceStart(String msg) {
+        traceTimeStack.push(System.currentTimeMillis());
+        LogUtil.i(TAG_TRACE_TIME, "startTime = " + System.currentTimeMillis() + " | " + msg);
+    }
+
+    public static void traceStop(String msg) {
+        if (traceTimeStack.isEmpty()) {
+            LogUtil.e("traceTimeStack.isEmpty()");
+            return;
+        }
+        long startTime = traceTimeStack.pop();
+        long durationTime = System.currentTimeMillis() - startTime;
+        LogUtil.i(TAG_TRACE_TIME, "endTime = " + System.currentTimeMillis() + " | " + msg);
+        LogUtil.i(TAG_TRACE_TIME, "durationTime = " + durationTime);
+    }
+
+    public static void traceReset() {
         traceTimeStack.clear();
     }
 
-    public static void startTraceTime(String msg) {
-        traceTimeStack.push(System.currentTimeMillis());
-        if (SHOW_LOG) {
-            Log.d(TAG_TRACE_TIME, msg + " time = " + System.currentTimeMillis());
-        }
+    /**
+     * 打印初始化信息
+     */
+    public static void printInit(String module) {
+        LogUtil.i(module, "初始化数据 -> " + module);
     }
 
-    public static void stopTraceTime(String msg) {
-        if (!traceTimeStack.isEmpty()) {
-            long time = traceTimeStack.pop();
-            long diff = System.currentTimeMillis() - time;
-            if (SHOW_LOG) {
-                Log.d(TAG_TRACE_TIME, "[" + diff + "]" + msg + " time = " + System.currentTimeMillis());
-            }
+    /**
+     * 打印异常信息
+     */
+    public static void printStackTrace(Throwable throwable) {
+        LogUtil.e(buildMessage("printStackTrace"), throwable);
+        throwable.printStackTrace();
+    }
+
+    /**
+     * 打印 ActivityManager
+     */
+    public static void printActivityList(LinkedList<Activity> activityList) {
+        StringBuilder builder = new StringBuilder();
+        builder.append("printActivityList | size -> ").append(activityList.size());
+        for (int i = 0; i < activityList.size(); i++) {
+            builder.append("\n").append(i).append(" -> ").append(activityList.get(i).getClass().getSimpleName());
         }
+        LogUtil.i(builder.toString());
     }
 
     /**
      * Building Message
      */
     private static String buildMessage(String msg) {
-        StackTraceElement caller = new Throwable().fillInStackTrace().getStackTrace()[2];
         StringBuilder builder = new StringBuilder();
-        if (SHOW_LOG_WITH_PATH) {
+        if (mShowPath) {
+            StackTraceElement caller = new Throwable().fillInStackTrace().getStackTrace()[2];
             builder.append(caller.getClassName());
             builder.append(".");
             builder.append(caller.getMethodName());
