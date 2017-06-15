16a02c7a566ece0a87fd89775a4f3ac8cb5ce45d
treason-rmbp <ma.tengfei2008@163.com>
2017-06-15 15:45:20 +0800

    TreCoreLib 添加 HelloUtil.getHI

---
+++ TreCoreLib/src/main/java/com/mjiayou/trecorelib/util/HelloUtil.java
@@ -0,0 +1,12 @@
+package com.mjiayou.trecorelib.util;
+
+/**
+ * Created by treason on 2017/6/15.
+ */
+
+public class HelloUtil {
+
+    public static String getHI() {
+        return "HI from HelloUtil in TreCoreLib!";
+    }
+}
--- app/src/main/java/com/mjiayou/trecore/MainActivity.java
+++ app/src/main/java/com/mjiayou/trecore/MainActivity.java
@@ -1,13 +1,29 @@
 package com.mjiayou.trecore;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import com.mjiayou.trecorelib.util.HelloUtil;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView mTvInfo;
+
+    private Context mContext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mContext = this;
+
+        // findViewById
+        mTvInfo = (TextView) findViewById(R.id.tv_info);
+
+        // mTvInfo
+        mTvInfo.append("\n" + HelloUtil.getHI() + "\n");
     }
 }
