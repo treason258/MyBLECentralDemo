50404c40919b7a76ced6b00bdaf48c48d52bf8e5
matengfei <matengfei@xin.com>
2018-09-25 15:20:20 +0800

    新增TC积累

---
+++ TreCoreLib/src/main/java/com/mjiayou/trecorelib/base/TCAdapter.java
@@ -0,0 +1,10 @@
+package com.mjiayou.trecorelib.base;
+
+import android.widget.BaseAdapter;
+
+/**
+ * Created by xin on 18/9/25.
+ */
+
+public abstract class TCAdapter extends BaseAdapter {
+}
---
+++ TreCoreLib/src/main/java/com/mjiayou/trecorelib/base/TCViewHolder.java
@@ -0,0 +1,14 @@
+package com.mjiayou.trecorelib.base;
+
+import android.view.View;
+
+/**
+ * Created by xin on 18/9/25.
+ */
+
+public abstract class TCViewHolder<T> {
+
+    protected abstract void findView(View view);
+
+    protected abstract void initView(T bean);
+}
