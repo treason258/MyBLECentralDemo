5edd23bddab3302499929c8793d5e8b74382da1c
matengfei <ma.tengfei2008@163.com>
2020-04-12 10:53:55 +0800

    HotFixActivity整理

--- app/src/main/java/com/mjiayou/trecore/test/hotfix/HotFixActivity.java
+++ app/src/main/java/com/mjiayou/trecore/test/hotfix/HotFixActivity.java
@@ -1,15 +1,10 @@
 package com.mjiayou.trecore.test.hotfix;
 
 import android.os.Bundle;
-import android.os.Environment;
 import android.view.View;
 
 import com.mjiayou.trecore.R;
 import com.mjiayou.trecorelib.base.TCActivity;
-
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
 
 public class HotFixActivity extends TCActivity {
 
@@ -24,50 +19,10 @@ public class HotFixActivity extends TCActivity {
     }
 
     public void crash(View view) {
-        // 崩溃代码
         CrashClass.crash();
     }
 
     public void fix(View view) {
-        // 将修复包复制到当前私有目录
-        File odex = getDir("odex", MODE_PRIVATE);
-        // 获取到补丁的名字
-        String name = "out.dex";
-        // 获取到修复包的完整名字
-        String filePath = new File(odex, name).getAbsolutePath();
-        File file = new File(filePath);
-        if (file.exists()) {
-            file.delete();
-        }
-        // 创建文件输入输出流
-        FileInputStream fileInputStream = null;
-        FileOutputStream fileOutputStream = null;
-        try {
-            // 起点
-            fileInputStream = new FileInputStream(new File(Environment.getExternalStorageDirectory(), name));
-            // 终点
-            fileOutputStream = new FileOutputStream(filePath);
-            int length = 0;
-            byte[] buffer = new byte[1024];
-            while ((length = fileInputStream.read(buffer)) != -1) {
-                fileOutputStream.write(buffer, 0, length);
-            }
-            File f = new File(filePath);
-            // 去加载dex文件 然后不定的dex文件与当前App的dex文件进行合并
-            HotFixUtils.loadDex(mContext);
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                if (fileInputStream != null) {
-                    fileInputStream.close();
-                }
-                if (fileOutputStream != null) {
-                    fileOutputStream.close();
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
+        HotFixUtils.loadDex(mContext);
     }
 }
--- app/src/main/java/com/mjiayou/trecore/test/hotfix/HotFixUtils.java
+++ app/src/main/java/com/mjiayou/trecore/test/hotfix/HotFixUtils.java
@@ -1,8 +1,11 @@
 package com.mjiayou.trecore.test.hotfix;
 
 import android.content.Context;
+import android.os.Environment;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.lang.reflect.Array;
 import java.lang.reflect.Field;
 import java.util.HashSet;
@@ -12,90 +15,120 @@ import dalvik.system.PathClassLoader;
 
 public class HotFixUtils {
 
-    private static HashSet<File> loadedDex = new HashSet<>();
+    public static String DEX_NAME = "out.dex";
+    public static String DIR_DEX = "dex";
+    public static String DIR_DEX_CACHE = "cache";
+
+    private static HashSet<File> mDexFiles = new HashSet<>();
 
     static {
-        loadedDex.clear();
+        mDexFiles.clear();
     }
 
     public static void loadDex(Context context) {
-        // 得到所有修复包
         if (context == null) {
             return;
         }
-        // 加载修复包
-        File filesDir = context.getDir("odex", Context.MODE_PRIVATE);
-        File[] files = filesDir.listFiles();
-        // 遍历 判断这个文件夹下面所有的文件是不是都是修复包
-        for (File file : files) {
-            if (file.getName().startsWith("classes") || file.getName().endsWith(".dex")) {
-                loadedDex.add(file);
+
+        // 获取修复文件在SD卡的文件名字
+        String dexFileName = HotFixUtils.DEX_NAME;
+        // 获取修复文件在SD卡的完整路径
+        File dexFileInSDCard = new File(Environment.getExternalStorageDirectory(), dexFileName);
+
+        // 创建修复包文件夹，如果不存在则创建
+        File dexFolder = context.getDir(DIR_DEX, Context.MODE_PRIVATE); // 将修复包复制到当前私有目录
+        if (!dexFolder.exists()) {
+            dexFolder.mkdirs();
+        }
+        // 如果修复包文件已存在则删除
+        File dexFileInApp = new File(dexFolder, dexFileName);
+        if (dexFileInApp.exists()) {
+            dexFileInApp.delete();
+        }
+
+        // 复制文件操作
+        FileInputStream fileInputStream = null;
+        FileOutputStream fileOutputStream = null;
+        try {
+            fileInputStream = new FileInputStream(dexFileInSDCard);
+            fileOutputStream = new FileOutputStream(dexFileInApp);
+            int length;
+            byte[] buffer = new byte[1024];
+            while ((length = fileInputStream.read(buffer)) != -1) {
+                fileOutputStream.write(buffer, 0, length);
             }
-        }
-
-        //创建一个odex的缓存目录
-        String optimizeDir = filesDir.getAbsolutePath() + File.separator + "cache_dex";
-        File fopt = new File(optimizeDir);
-        if (!fopt.exists()) {
-            fopt.mkdirs();
-        }
-
-        for (File dex : loadedDex) {
-            // http://androidxref.com/8.0.0_r4/xref/libcore/dalvik/src/main/java/dalvik/system/BaseDexClassLoader.java
-            // http://androidxref.com/8.0.0_r4/xref/libcore/dalvik/src/main/java/dalvik/system/DexPathList.java
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
             try {
-                //// 第一步 先获取当前应用的 dexElements 的对象
-
-                PathClassLoader pathClassLoader = (PathClassLoader) context.getClassLoader();
-                // 通过反射获取到BaseDexClassLoader类对象
-                Class<?> BaseDexClassLoader = Class.forName("dalvik.system.BaseDexClassLoader");
-
-                // 获取 BaseDexClassLoader 类中的 pathList 的反射对象
-                Field pathListFiled = BaseDexClassLoader.getDeclaredField("pathList");
-                // 先打开权限
-                pathListFiled.setAccessible(true);
-                // 获取 pathList 这个变量在当前类加载器中的值
-                Object pathListValue = pathListFiled.get(pathClassLoader);
-
-                // 获取 dexElements 成员变量的反射对象
-                Field dexElementsField = pathListValue.getClass().getDeclaredField("dexElements");
-                // 打开权限
-                dexElementsField.setAccessible(true);
-                // 获取当前应用的 dexElements 在当前应用中的值
-                Object systemDexElementValues = dexElementsField.get(pathListValue);
-
-
-                //// 第二步 获取修复包的 dexElements 的对象
-                DexClassLoader dexClassLoader = new DexClassLoader(dex.getAbsolutePath(), optimizeDir, null, context.getClassLoader());
-                // 获取到 pathList 在修复包中的值
-                Object myPathListValue = pathListFiled.get(dexClassLoader);
-                Object fixDexElementValues = dexElementsField.get(myPathListValue);
-
-                //// 第三步 合并数组
-                // 得到连个数组的长度
-                int length = Array.getLength(systemDexElementValues);
-                int fixLength = Array.getLength(fixDexElementValues);
-                int newLength = length + fixLength;
-                // 创建一个新的数组
-                Class<?> componentType = systemDexElementValues.getClass().getComponentType();
-                // 创建了一个 Element[] 数组
-                Object newElementArray = Array.newInstance(componentType, newLength);
-                // 将两个数组中的内容收入到新的数组中
-                for (int x = 0; x < newLength; x++) {
-                    // 首先放修复包的dex文件
-                    if (x < fixLength) {
-                        Array.set(newElementArray, x, Array.get(fixDexElementValues, x));
-                    } else {
-                        Array.set(newElementArray, x, Array.get(systemDexElementValues, x - fixLength));
-                    }
+                if (fileInputStream != null) {
+                    fileInputStream.close();
                 }
-
-                //// 将合并好的 dexElement 的数组值赋值给当前应用的 dexElement 的成员变量
-                dexElementsField.set(pathListValue, newElementArray);
-
+                if (fileOutputStream != null) {
+                    fileOutputStream.close();
+                }
             } catch (Exception e) {
                 e.printStackTrace();
             }
+        }
+
+        // 遍历判断这个文件夹下面所有的文件是不是都是修复包
+        for (File dexFile : dexFolder.listFiles()) {
+            if (dexFile.getName().startsWith("classes") || dexFile.getName().endsWith(".dex")) {
+                mDexFiles.add(dexFile);
+            }
+        }
+
+        // 创建一个dex的缓存目录
+        String optimizedFolderPath = dexFolder.getAbsolutePath() + File.separator + DIR_DEX_CACHE;
+        File optimizedFile = new File(optimizedFolderPath);
+        if (!optimizedFile.exists()) {
+            optimizedFile.mkdirs();
+        }
+
+        // 遍历修复包逐个处理
+        try {
+            for (File dexFile : mDexFiles) {
+                //// 第一步、先获取当前应用的 dexElements 对象
+                // http://androidxref.com/8.0.0_r4/xref/libcore/dalvik/src/main/java/dalvik/system/BaseDexClassLoader.java
+                // http://androidxref.com/8.0.0_r4/xref/libcore/dalvik/src/main/java/dalvik/system/DexPathList.java
+                PathClassLoader pathClassLoader = (PathClassLoader) context.getClassLoader(); // 获取当前应用的 pathClassLoader
+                Class<?> BaseDexClassLoader = Class.forName("dalvik.system.BaseDexClassLoader"); // 通过反射获取到 BaseDexClassLoader 类对象
+
+                Field pathListFiled = BaseDexClassLoader.getDeclaredField("pathList"); // 获取 BaseDexClassLoader 类中的 pathList 成员变量的反射对象
+                pathListFiled.setAccessible(true); // 打开权限
+                Object systemPathListValue = pathListFiled.get(pathClassLoader); // 获取 pathList 这个变量在当前系统 pathClassLoader 中的值
+
+                Field dexElementsField = systemPathListValue.getClass().getDeclaredField("dexElements"); // 获取 DexPathList 类中的 dexElements 成员变量的反射对象
+                dexElementsField.setAccessible(true); // 打开权限
+                Object systemDexElementsValue = dexElementsField.get(systemPathListValue); // 获取 dexElements 在当前系统 systemPathListValue 中的值
+
+                //// 第二步、获取修复包的 dexElements 对象
+                DexClassLoader fixDexClassLoader = new DexClassLoader(dexFile.getAbsolutePath(), optimizedFolderPath, null, context.getClassLoader());
+                Object fixPathListValue = pathListFiled.get(fixDexClassLoader); // 获取到 pathList 这个变量在修复包 fixDexClassLoader 中的值
+                Object fixDexElementsValue = dexElementsField.get(fixPathListValue); // 获取 dexElements 这个变量在修复包 fixPathListValue 中的值
+
+                //// 第三步、合并数组
+                // 得到数组的长度
+                int systemElementsLength = Array.getLength(systemDexElementsValue);
+                int fixElementsLength = Array.getLength(fixDexElementsValue);
+                int newLength = systemElementsLength + fixElementsLength;
+                Class<?> componentType = systemDexElementsValue.getClass().getComponentType(); // 获取 Elements 对象类型
+                Object newElementArray = Array.newInstance(componentType, newLength); // 创建 Element 数组
+                // 将两个数组中的内容收入到新的数组中
+                for (int i = 0; i < newLength; i++) {
+                    if (i < fixElementsLength) { // 首先放修复包的dex文件
+                        Array.set(newElementArray, i, Array.get(fixDexElementsValue, i));
+                    } else {
+                        Array.set(newElementArray, i, Array.get(systemDexElementsValue, i - fixElementsLength));
+                    }
+                }
+
+                //// 第四步、将合并好的 dexElement 的数组值赋值给当前应用的 dexElement 的成员变量
+                dexElementsField.set(systemPathListValue, newElementArray);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
